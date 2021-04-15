8128e1be9bcda4a3794826267ff8e0dfcf7bf34f
mawir157 <jamesmthompson2@gmail.com>
2021-04-15 12:07:09 +0100

    Add permuted congruential generator

---
+++ rand/pcg.go
@@ -0,0 +1,39 @@
+package rand
+
+import (
+	"time"
+)
+
+type PermConGen struct {
+	state      uint64
+	multiplier uint64
+	increment  uint64
+}
+
+func PCGInit() *PermConGen {
+	return &PermConGen{multiplier: 6364136223846793005,
+	                   increment:  1442695040888963407,
+	                  }
+}
+
+func (rng *PermConGen) Seed(seed int) {
+	if seed == 0 {
+		seed = int(time.Now().UnixNano())
+	}
+
+	rng.state = uint64(seed) + rng.increment
+}
+
+func (rng *PermConGen) Next() int {
+		x := rng.state
+		count := uint(x >> 59)
+
+		rng.state = x * rng.multiplier + rng.increment
+		x = x ^ (x >> 18)
+
+		return rotr32(uint32(x >> 27), count)
+}
+
+func rotr32(x uint32, r uint) int {
+	return int( (x >> r) | (x << (-r & 31)) )
+}
