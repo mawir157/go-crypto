5825ba6720faa6f061b23d9cde499db842ee0613
mawir157 <jamesmthompson2@gmail.com>
2021-04-29 21:52:13 +0100

    diffie hellman :key:

---
+++ dh/dh.go
@@ -0,0 +1,37 @@
+package dh // diffie hellman
+
+import (
+	"math/big"
+)
+
+type DiffieHellman struct {
+	p, g big.Int
+}
+
+func DiffHell(p, g big.Int) DiffieHellman {
+	return DiffieHellman{p:p, g:g}
+}
+
+func ParseToBigInt(s string) (i big.Int) {
+	i.SetString(s, 10)
+
+	return 
+}
+
+func ParseToBigIntHex(s string) (i big.Int) {
+	i.SetString(s, 16)
+
+	return 
+}
+
+func (dh DiffieHellman) ToPublic(a *big.Int) (A big.Int) {
+	A.Exp(&dh.g, a, &dh.p)
+
+	return
+}
+
+func (dh DiffieHellman) ToShared(B, a *big.Int) (S big.Int) {
+	S.Exp(B, a, &dh.p)
+
+	return
+}
